aeb1c86d0b1562aa53ea2be43715aa024d5d0bff
haraldnilsen <harald_998@hotmail.com>
2024-06-06 19:15:33 +0200

    :sparkles: init backend

---
+++ backend/LoanCalculator/Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+using System.Globalization;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using LoanCalculator;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var configuration = builder.Configuration; 
+
+builder.Services.AddDbContext<LoanDbContext>(options =>
+    options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
