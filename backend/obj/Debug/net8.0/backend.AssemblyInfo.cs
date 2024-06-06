d82f3abd3ac4a7ebf8b53334b0854db1d4cf3bea
haraldnilsen <harald_998@hotmail.com>
2024-06-06 17:21:09 +0200

    :arrow_up: add inmemory package

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09583afed5ad8115b0f1e05b567b0627aa998a24")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2390aeb3f6be9bb2600b3aeb0d0b3fbef9706664")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
