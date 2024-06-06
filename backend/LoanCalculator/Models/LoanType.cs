5a8b8913661f5cb075e9ce3a9f90de62a15d2c8d
haraldnilsen <harald_998@hotmail.com>
2024-06-06 19:29:13 +0200

    :card_file_box: auto increment primary key

--- backend/LoanCalculator/Models/LoanType.cs
+++ backend/LoanCalculator/Models/LoanType.cs
@@ -1,9 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace LoanCalculator.Models;
 
 public class LoanType 
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id {get; set;}
     public string? Name {get; set;}
-
     public long Interest {get; set;}
 }
