acfdd613d884490b83bb8ef8eae3a5dfb719a12e
haraldnilsen <harald_998@hotmail.com>
2024-06-11 15:46:06 +0200

    :lipstick: updated styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,8 +3,14 @@ import LoanCalculator from "./components/LoanCalculator";
 
 const App: React.FC = () => {
   return (
-    <div className="flex flex-col w-screen items-center text-center">
-      <h1 className="my-6">Loan Calculator</h1>
+    <div className="flex bg-content flex-col w-screen items-center">
+      <header className="header py-16">
+        <h1 className="text-bkg mb-6 font-medium">Loan Calculator</h1>
+        <h2 className="text-secondary font-semibold">
+          Calculate how much you rent will cost you. Input the amount, payback
+          time and which type of loan you have.
+        </h2>
+      </header>
       <LoanCalculator />
     </div>
   );
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,30 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bkg: "var(--color-bkg)",
+        content: "var(--color-content)",
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+        accent: "var(--color-accent)",
+        contrast: "var(--color-contrast)",
+      },
+      keyframes: {
+        spin360: {
+          "0%": { transform: "rotate(0deg)" },
+          "100%": { transform: "rotate(-360deg)" },
+        },
+      },
+      animation: {
+        spin360: "spin360 0.65s linear",
+      },
+    },
+  },
+  variants: {
+    extend: {
+      animation: ["hover"],
+    },
   },
   plugins: [],
 };
